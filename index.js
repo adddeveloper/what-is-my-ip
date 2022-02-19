4b559d2d288c445e6bd48fb733a9d93a6e610a71
Adnan <77362547+adddeveloper@users.noreply.github.com>
2022-02-19 09:47:49 -0400

    v-0.0.02.19

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-var sbar, sbutton, content, img;
-img = document.createElement("img")
+var sbar, sbutton, icontent, pcontent;
 sbutton = document.getElementById("search_button");
 sbar = document.getElementById("search_bar");
-content = document.getElementById("content");
+icontent = document.getElementById("i_content").childNodes;
+pcontent = document.getElementById("p_content").childNodes;
+
+fetch("http://ipwhois.app/json/"+sbar.value+"?objects=country,city,ip,country_flag")
+    .then(res=>res.json())
+    .then((data) => {
+        pcontent[0].innerHTML = "IP: "+ "<b>" + data.ip + "</b>";
+        pcontent[1].innerHTML = "Country:  "+ "<b>" + data.country + "</b>";
+        pcontent[2].innerHTML = "City: " + "<b>" + data.city + "</b>";
+        // image part
+        icontent[0].src=data.country_flag;
+        icontent[0].style.height ="auto";
+        pcontent[2].scrollIntoView()
+        // scrollTo(0, window.innerHeight)
+});
 
 sbutton.addEventListener("click", ()=>{
     fetch("http://ipwhois.app/json/"+sbar.value+"?objects=country,city,ip,country_flag")
     .then(res=>res.json())
     .then((data) => {
-        content.textContent = data.country +""+ data.ip +""+ data.city;
-        console.log(data);
-        img.src=data.country_flag;
+        pcontent[0].innerHTML = "IP: "+ "<b>" + data.ip + "</b>";
+        pcontent[1].innerHTML = "Country:  "+ "<b>" + data.country + "</b>";
+        pcontent[2].innerHTML = "City: " + "<b>" + data.city + "</b>";
+        // image part
+        icontent[0].src=data.country_flag;
+        icontent[0].style.height ="auto";
+        pcontent[2].scrollIntoView()
+        // scrollTo(0, window.innerHeight)
     });
-    document.body.appendChild(img)
 })
